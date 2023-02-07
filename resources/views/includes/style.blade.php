c647c5b54cd48ba95ab4e9a593767fc9a96b4737
Momoy <irosyadi16@gmail.com>
2023-02-07 21:48:39 +0700

    favicon add

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,2 +1,5 @@
+<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
 <link href="/style/main.css" rel="stylesheet" />
